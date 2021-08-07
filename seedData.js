51b67f17de873d86c3cc9d388469dfa3bd86ae28
Amogh Godbole <amoghaugust23@gmail.com>
2021-08-07 10:54:00 +0530

    Add seedData. Make minor updates to enquiryList and businessList

---
+++ seedData.js
@@ -0,0 +1,89 @@
+const seed = [
+  {
+    location: "Andheri",
+    domains: ["Health", "Medical", "General"],
+    businessName: "Some business 1",
+    id: 1,
+  },
+  {
+    location: "Churchgate",
+    domains: ["Sports", "Clothing"],
+    businessName: "Some business 2",
+    id: "610cf6851605495770acdf2b",
+  },
+  {
+    location: "Andheri",
+    domains: ["Software", "Electronics"],
+    businessName: "Some business 8",
+    id: "610cf81719303111fcdf4771",
+  },
+  {
+    location: "Vile Parle",
+    domains: ["Hardware", "Plumbing", "Carpentry"],
+    businessName: "Some business 3",
+    id: 4,
+  },
+  {
+    location: "Andheri",
+    domains: ["Electronics"],
+    businessName: "Some business 4",
+    id: 5,
+  },
+];
+
+const enquiries = [
+  {
+    customerName: "Amogh",
+    enquiry: "Do you have this hellooo",
+    id: 1,
+  },
+  {
+    customerName: "Arnav",
+    enquiry: "Do you have",
+    id: 2,
+  },
+  {
+    customerName: "Soham",
+    enquiry: "Do you Fuck off",
+    id: 3,
+  },
+  {
+    customerName: "Ishan",
+    enquiry: "Do you have Puneri Misal",
+    id: 4,
+  },
+];
+
+// Western railway stations
+const locations = [
+  "Andheri",
+  "Bandra",
+  "Bhayander",
+  "Borivali",
+  "Charni Rd",
+  "Churchgate",
+  "Dadar",
+  "Dahisar",
+  "Elphinstone Rd",
+  "Goregaon",
+  "Grant Rd",
+  "Jogeshwari",
+  "Kandivali",
+  "Khar Rd",
+  "Lower Parel",
+  "Mahalakshmi",
+  "Mahim",
+  "Malad",
+  "Marine Lines",
+  "Matunga Rd",
+  "Mira Rd",
+  "Mumbai Central",
+  "Naigaon",
+  "Nalla Sopara",
+  "Santa Cruz",
+  "Vasai Rd",
+  "Vile Parle",
+  "Virar",
+];
+
+module.exports = { seed, enquiries, locations };
